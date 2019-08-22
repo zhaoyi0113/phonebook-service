de966d5f078ba8174082212d38cc7f3158a0d8ca
Joey Zhao <zhaoyi0113@gmail.com>
2019-08-22 14:53:32 +1000

    add postgres container

---
+++ sql/create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE contact (
+    id serial,
+    username VARCHAR(40),
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    email   VARCHAR(40),
+    pwd VARCHAR(16),
+    phone VARCHAR(20),
+    user_status VARCHAR(10)
+);
